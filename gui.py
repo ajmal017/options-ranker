f2f3b18e06796d7e77aee087f00e66d5f6f008cb
Milki Negeri <milki10mn@gmail.com>
2020-07-05 15:42:28 -0500

    added scrolling capability to options page

--- gui.py
+++ gui.py
@@ -1,8 +1,7 @@
 import tkinter as tk
-from tkinter import ttk
-LARGEFONT = ("Verdana", 35)
 from symbols import symbols
 
+menu_options = ["volume", "open-interest"]
 
 class tkinterApp(tk.Tk):
     # __init__ function for class tkinterApp
@@ -10,41 +9,25 @@ class tkinterApp(tk.Tk):
         # __init__ function for class Tk
         tk.Tk.__init__(self, *args, **kwargs)
 
-        # creating a container
-        """container = tk.Frame(self)   
-        container.pack(side = "top", fill = "both", expand = True)  
-   
-        container.grid_rowconfigure(0, weight = 1) 
-        container.grid_columnconfigure(0, weight = 1) """
         window_h = self.winfo_screenheight()
         window_w = self.winfo_screenwidth()
         self.geometry("%dx%d+0+0" % (window_w, window_h))
-        main_frame = tk.Frame(self)
-        main_frame.place(relwidth=1, relheight=1)
+        self.main_frame = tk.Frame(self)
+        self.main_frame.place(relwidth=1, relheight=1)
 
-        # initializing frames to an empty array
-        self.frames = {}
-
-        # iterating through a tuple consisting
-        # of the different page layouts
-        for F in (StartPage, Page1, Page2):
-
-            frame = F(main_frame, self)
-
-            # initializing frame of that object from
-            # startpage, page1, page2 respectively with
-            # for loop
-            self.frames[F] = frame
-
-            #frame.grid(row = 0, column = 0, sticky ="nsew")
-            frame.place(relwidth=1, relheight=1)
-
-        self.show_frame(StartPage)
+        self.show_frame(StartPage, "")
 
     # to display the current frame passed as
     # parameter
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+    def show_frame(self, page, detail):
+        if detail == "":
+            self.main_frame = tk.Frame(self)
+            self.main_frame.place(relwidth=1, relheight=1)
+            frame = page(self.main_frame, self)
+        else:
+            frame = page(self.main_frame, self, detail) 
+        frame.place(relwidth=1, relheight=1)
+        #frame = self.frames[cont]
         frame.tkraise()
 
 
@@ -52,17 +35,12 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        # label of frame Layout 2
-        #label = ttk.Label(self, text ="Startpage")
-
-        # putting the grid in its place by using
-        # grid
-        #label.grid(row = 0, column = 4, padx = 10, pady = 10)
+        #Adds menubotton with various options
         mb = tk.Menubutton(self, text="Sort options by", width=10)
         mb.place(relx=0.6, rely=0.09)
         mb.menu = tk.Menu(mb, tearoff=0)
         mb["menu"] = mb.menu
-        mb.menu.add_checkbutton(label="Highest volume", command=lambda: controller.show_frame(Page1))
+        mb.menu.add_checkbutton(label=menu_options[0], command=lambda: controller.show_frame(OptionsPage, menu_options[0]))
         mb.menu.add_checkbutton(label="Highest Open Interest")
 
         self.entry = tk.Entry(self, width=20, bg="white")
@@ -99,51 +77,65 @@ class StartPage(tk.Frame):
             self.listbox.insert('end', item)
 
     def selected(self, event):
-        #chosen = event.widget.get(event.widget.curselection())
         chosen = self.listbox.get(self.listbox.curselection())
         self.entry.delete(0, len(chosen))
         self.entry.insert(0, chosen)
 
+#creates a frame with scrolling capabilities
+class ScrollFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent) # create a frame (self)
 
-# second window frame page1
-class Page1(tk.Frame):
+        self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
+        self.view = tk.Frame(self.canvas, background="#ffffff") 
+        #creates a scrollbar
+        self.scroll= tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scroll.set)
 
-    def __init__(self, parent, controller):
+        self.scroll.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas_window = self.canvas.create_window((4,4), window=self.view, anchor="nw", tags="self.view")
+
+        #binds and event whenever size of view frame changes
+        self.view.bind("<Configure>", self.onFrameConfigure)
+        self.canvas.bind("<Configure>", self.onCanvasConfigure)
+
+        self.onFrameConfigure(None)
+
+    def onFrameConfigure(self, event):                                              
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))                 
+
+    def onCanvasConfigure(self, event):
+        '''Reset the canvas window to encompass inner frame when required'''
+        canvas_width = event.width
+        self.canvas.itemconfig(self.canvas_window, width = canvas_width)            
+
+
+class OptionsPage(tk.Frame):
+    def __init__(self, parent, controller, detail):  #are these, parameters rigth?????
 
         tk.Frame.__init__(self, parent)
-        # button to show frame 2 with text
-        # layout2
-        back_button = tk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage))
+        back_button = tk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage, ""))
         back_button.place(relx=0, rely=0)
 
-        window = tk.Frame(self, bg='blue') 
-        window.place(relx=0.2, rely=0.2, relwidth=0.75, relheight=0.75)
+        new_frame = tk.Frame(controller, bg="grey")
+        new_frame.place(relx=0.5, rely=0.5, relwidth=0.75, relheight=0.75, anchor="center")
 
+        self.scrollFrame = ScrollFrame(new_frame) # add a new scrollable frame.
+        
+        # Now add some controls to the scrollframe. 
+        # NOTE: the child controls are added to the view port (scrollFrame.viewPort, NOT scrollframe itself)
+        for row in range(100):
+            a = row
+            tk.Label(self.scrollFrame.view, text="%s" % row, width=3, borderwidth="1", 
+                     relief="solid").grid(row=row, column=0)
+            t="this is the second column for row %s" %row
+            tk.Button(self.scrollFrame.view, text=t, command=lambda x=a: self.printMsg("Hello " + str(x))).grid(row=row, column=1)
 
-# third window frame page2
-class Page2(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page 2", font=LARGEFONT)
-        label.grid(row=0, column=4, padx=10, pady=10)
-
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text="Page 1",
-                             command=lambda: controller.show_frame(Page1))
-
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        # button to show frame 3 with text
-        # layout3
-        button2 = ttk.Button(self, text="Startpage",
-                             command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
+        # when packing the scrollframe, we pack scrollFrame itself (NOT the viewPort)
+        self.scrollFrame.pack(side="top", fill="both", expand=True)
+    
 
 
 # Driver Code
