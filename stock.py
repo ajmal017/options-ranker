228193d0d4b65bee1681c51744ca0228b9dcd6ca
Milki Negeri <milki10mn@gmail.com>
2020-07-11 14:38:50 -0500

    retrived more company data from api

--- stock.py
+++ stock.py
@@ -6,9 +6,12 @@ from api_key import key
 class Stock:
     options_endpoint = "https://finnhub.io/api/v1/stock/option-chain"
     quote_endpoint = "https://finnhub.io/api/v1/quote"
+    profile_endpoint = "https://finnhub.io/api/v1/stock/profile2"
 
     def __init__(self, ticker):
         self.ticker = ticker
+        params = {"symbol": self.ticker, "token": key}
+        self.profile = requests.get(self.profile_endpoint, params=params).json()
 
     #returns the current price of the stock
     def current_price(self):
